77942557c1d076aed3dbef3af17d1dda65e44de7
nitish nagabhairava <nitishrockson@gmail.com>
2017-11-18 11:52:39 +0100

    Create JuliaJames.js
    julia james similar to fizzbuzz but using while loop

---
+++ JuliaJames.js
@@ -0,0 +1,18 @@
+/*
+ * Programming Quiz: JuliaJames similar to Fizzbuzz
+ */
+
+var x = 1;
+
+while (x < 21) {
+   if(x%3 ===0 && x%5 ===0){
+       console.log("JuliaJames");
+    }else if(x%5 ===0 ){
+       console.log("James")
+    }else if(x%3 ===0){
+        console.log("Julia");
+    }else{
+        console.log(x)
+    }
+    x++;
+}
