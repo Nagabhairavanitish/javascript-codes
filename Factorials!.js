60232df89cecc41ccb064256cd6f9f3fafad51c7
nitish nagabhairava <nitishrockson@gmail.com>
2017-11-18 14:34:40 +0100

    Create Factorials!.js

---
+++ Factorials!.js
@@ -0,0 +1,21 @@
+solution = 12;  
+for (var fact = 1; fact < 12; fact ++) {
+  solution *= fact;
+}
+ console.log(solution);
+
+
+/* Factorial of 12, solution starts at 12. solution* factwill first set 12*1. 
+1. solution* fact : 12*1 = 12
+2. solution*fact : 12*2 = 24 
+3. solution*fact :24*3 = 72
+4. solution *fact : 72*4 = 288
+5. solution *fact : 288 *5 = 1,440
+6. solution *faonct : 1440 * 6 = 8,640
+7. solution *facton : 8640 * 7 = 60,480
+8. solution *fact : 60,480 * 8 = 483,840
+9. solution *fact : 483,840 * 9 = 4,354,560
+10. solution *fact : 4,354,560 * 10 = 43,545,600
+11. solution *fact : 43,545,600 * 11 = 479,001,600
+
+*/
