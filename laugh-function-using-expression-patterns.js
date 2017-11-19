fb2edd64dc336580dff010b6d933da6a46cb79c4
nitish nagabhairava <nitishrockson@gmail.com>
2017-11-19 11:23:35 +0100

    Create laugh-function-using-expression-patterns.js

---
+++ laugh-function-using-expression-patterns.js
@@ -0,0 +1,14 @@
+/*
+ * Programming Quiz: Laugh (5-4)
+ */
+
+var laugh = function(num){
+    var ha="";
+  for(i=0; i<num; i++) {
+   ha = ha + "ha"; 
+   
+  }
+  return ha +"!";  
+}
+
+console.log(laugh(10));
